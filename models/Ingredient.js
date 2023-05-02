b1832fac185d84fa7a2a9f73d62c592c889025ce
Megan Glorioso <meganglorioso2016@gmail.com>
2023-05-01 19:29:10 -0700

    added ingredient model

---
+++ models/Ingredient.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Ingredient extends Model {}
+
+Ingredient.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        measurement_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        amount: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+        recipe_id: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'recipe',
+                key: 'id',
+            },
+        },
+    },
+
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'ingredient',
+}
+);
+
+module.exports = Ingredient;
