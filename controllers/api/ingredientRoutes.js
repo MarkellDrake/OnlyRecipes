d29f26f42f3208c2307feaa4511f800493487395
zacharydserafin <zacharydserafin@gmail.com>
2023-05-04 22:17:23 -0500

    Set Up Authentication in Routes and User Model

--- controllers/api/ingredientRoutes.js
+++ controllers/api/ingredientRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { Ingredient } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
         const ingredientData = await Ingredient.create(req.body);
         res.status(200).json(ingredientData);
--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { Recipe } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
         const recipeData = await Recipe.create(req.body);
         res.status(200).json(recipeData);
