8ee26a60c7c693d6b50d71bb1822b07d8b4553d5
zacharydserafin <zacharydserafin@gmail.com>
2023-05-04 21:03:54 -0500

    Added Home Routes for Handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,95 @@
+const router = require('express').Router();
+const { Comment, User, Recipe, Ingredient } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const recipeData = await Recipe.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Ingredient,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    include: ['id'],
+                },
+            ]
+        });
+
+        const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+
+        res.render('homepage', {
+            recipes
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/recipe/:id', async (req, res) => {
+    try {
+        const recipeData = await Recipe.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Ingredient,
+                    attributes: ['name', 'measurement_type', 'amount'],
+                },
+                {
+                    model: Comment,
+                    include: ['id', 'name', 'comment'],
+                },
+            ]
+        });
+
+        const recipe = recipeData.get({ plain: true });
+
+        res.render('recipe', {
+            recipe
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [
+                {
+                    model: Recipe
+                },
+                {
+                    model: Comment
+                }
+            ]
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+});
+
+module.exports = router;
